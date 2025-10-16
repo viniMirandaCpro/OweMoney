d73447aa9a44de0b70b5f06af8ee089c27db7131
Vinicius Miranda <vinimirandapro220@gmail.com>
2025-10-16 18:37:15 -0300

    first commit

---
+++ lib/pages/owe_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class OwePage extends StatefulWidget {
+
+  const OwePage({ super.key });
+
+  @override
+  State<OwePage> createState() => _OwePageState();
+}
+
+class _OwePageState extends State<OwePage> {
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text(''),),
+           body: Container(),
+       );
+  }
+}
